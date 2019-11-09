c2efa419cc6fcba10af187d70987d9473b122bcd
Jen <jmbabaran@gmail.com>
2019-11-09 13:39:40 +1100

    Fibonacci upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Fibo(7); //output 13
+
+
+            Console.WriteLine("Result: " + result);
+        }
+
+        public static int Fibo(int n)
+        {
+            if (n <=1)
+            {
+                return n;
+            } else
+            {
+                return Fibo(n - 1) + Fibo(n - 2);
+            }
+
+        }
+    }
+}
